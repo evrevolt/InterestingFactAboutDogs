285679cc9a51b1c992cda9b7fab591a9bb092ecc
Gennady Vedernikov <revoltlight@icloud.com>
2022-11-04 20:41:08 +0300

    Finish project

--- InterestingFactAboutDogs/ViewController/ViewController.swift
+++ InterestingFactAboutDogs/ViewController/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
     
     var facts: [String] = []
     
-    
     @IBOutlet var activiryIndicator: UIActivityIndicatorView!
     @IBOutlet var factLabel: UILabel!
     
@@ -26,61 +25,24 @@ class ViewController: UIViewController {
         //Настраиваем ромашку
         activiryIndicator.startAnimating()
         activiryIndicator.hidesWhenStopped = true
-        DispatchQueue.main.async {
-            self.showLabel()
-        }
     }
     
     @IBAction func nextFactButtonTapped() {
-        
-        let result = facts.randomElement()
-        factLabel.text = result
-        
+        factLabel.text = facts.randomElement()
     }
     
     //Загружаем данные из сети
     private func fetchFactAboutDogs() {
         NetworkManager.shared.fetchFacts(url: Link.url.rawValue) { [unowned self] strings in
             self.facts = strings
+            
+            //Ждем загрузки данных
+            DispatchQueue.main.async {
+                self.factLabel.text = self.facts.randomElement()
+                self.factLabel.isHidden = false
+                self.activiryIndicator.stopAnimating()
+            }
         }
     }
-    
-    private func showLabel() {
-        
-        DispatchQueue.main.async {
-            self.factLabel.text = self.facts.randomElement()
-            self.factLabel.isHidden = false
-            self.activiryIndicator.stopAnimating()
-        }
-        
-    }
-        
-        
-        
-        
-        //    func downloadFacts() {
-        //        guard let url = URL(string: "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all") else { return }
-        //        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
-        //            guard let data = data else {
-        //                print(error?.localizedDescription ?? "No error description")
-        //                return
-        //            }
-        //
-        //            do {
-        //                let fact = try JSONDecoder().decode([InterestingFact].self, from: data)
-        //                DispatchQueue.main.async {
-        //
-        //                    self?.factLabel.text = getStrings(from: fact).randomElement()
-        //                    self?.activiryIndicator.stopAnimating()
-        //                    self?.factLabel.isHidden = false
-        //
-        //                    self?.facts = fact
-        //                }
-        //            } catch let error {
-        //                print(error)
-        //            }
-        //        }.resume()
-        //    }
-        
 }
 
